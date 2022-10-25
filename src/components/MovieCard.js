cd794bb840a1ec5640d064ca0156298a6ebe7d30
laibasarwar <sarwar.laiba@gmail.com>
2022-10-25 18:06:39 -0400

    added info

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,45 @@
 import React from "react";
+// import MovieImage from "./MovieImage";
 
-function MovieCard({ movie }) {
+function MovieCard({
+  awards,
+  companies,
+  contentRating,
+  directors,
+  fullTitle,
+  genres,
+  id,
+  imDbRating,
+  image,
+  languages,
+  plot,
+  releaseDate,
+  stars,
+  title,
+  trailer,
+  writers,
+  year,
+}) {
   return (
-    <div>
-      <h2>{movie.title}</h2>
+    <div className="MovieCard">
+      <h2>{title}</h2>
+      <p> Awards: {awards} </p>
+      <p> Companies: {companies} </p>
+      <p> Content RatingL{contentRating}</p>
+      <p> Director: {directors}</p>
+      <p> Full Title: {fullTitle}</p>
+      <p> Genres: {genres}</p>
+      <p> ID: {id}</p>
+      <p> imDb Rating: {imDbRating}</p>
+      <p> Image:{image}</p>
+      <p> Languages:{languages}</p>
+      <p> Plot:{plot}</p>
+      <p> Release Date: {releaseDate}</p>
+      <p> Stars: {stars}</p>
+      <p> Title: {title}</p>
+      <p> Trailer: {trailer}</p>
+      <p> Writer: {writers}</p>
+      <p> Year: {year}</p>
     </div>
   );
 }
--- src/pages/MovieListing.js
+++ src/pages/MovieListing.js
@@ -1,34 +1,105 @@
 import axios from "axios";
 import React, { useEffect, useState, useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
 import { IMDB_APP_API_KEY } from "../API_KEYS";
 import Header from "../components/Header";
 import MovieCard from "../components/movieCard";
 
-const IMDBURL = `https://imdb-api.com/en/API/Top250Movies/${IMDB_APP_API_KEY}`;
+const IMDBURL = `https://imdb-api.com/en/API/Title/${IMDB_APP_API_KEY}/tt1375666/FullActor,FullCast,Posters,Images,Trailer,Ratings,Wikipedia`;
 
 function Movies() {
   const [movieData, setMovieData] = useState({});
+  const [title, setTitle] = useState("tt1375666");
+  const [searchParams] = useSearchParams();
+
+  //10 fav movies
+  //The Shawshank Redemption, Forrest Gump, The Lion King, Joker, Coco, Good Will Hunting, 3 Idiots, Room, Spotlight, The Help
+
+  console.log("searchParams", searchParams.get("title"));
 
   useEffect(() => {
+    const titleToQuery = searchParams.get("title") || title;
+    setTitle(titleToQuery);
     axios
-      .get(`https://imdb-api.com/en/API/Top250Movies/${IMDB_APP_API_KEY}`)
+      .get(
+        `https://imdb-api.com/en/API/Title/${IMDB_APP_API_KEY}/${titleToQuery}/FullActor,FullCast,Posters,Images,Trailer,Ratings,Wikipedia`
+      )
       .then(function (mresponse) {
         setMovieData(mresponse.data);
       })
       .catch(function (error) {
+        console.warn(error);
         setMovieData({});
       });
   }, []);
 
-  console.log("Title", movieData.items);
+  console.log("Title", movieData);
 
-  //10 fav movies
-  //The Shawshank Redemption, Forrest Gump, The Lion King, Joker, Coco, Good Will Hunting, 3 Idiots, Room, Spotlight, The Help
+  const {
+    awards,
+    companies,
+    contentRating,
+    directors,
+    fullTitle,
+    genres,
+    id,
+    imDbRating,
+    image,
+    languages,
+    plot,
+    releaseDate,
+    stars,
+    // title,
+    // trailer,
+    writers,
+    year,
+  } = useMemo(() => {
+    const movieMain = movieData || {};
+    return {
+      awards: movieMain.awards,
+      companies: movieMain.companies,
+      contentRating: movieMain.contentRating,
+      directors: movieMain.directors,
+      fullTitle: movieMain.fullTitle,
+      genres: movieMain.genres,
+      id: movieMain.id,
+      imDbRating: movieMain.imDbRating,
+      image: movieMain.image,
+      languages: movieMain.languages,
+      plot: movieMain.plot,
+      releaseDate: movieMain.releaseDate,
+      stars: movieMain.stars,
+      // title: movieMain.title,
+      // trailer: movieMain.trailer,
+      writers: movieMain.writers,
+      year: movieMain.year,
+    };
+  }, [movieData]);
 
   return (
     <div>
       <Header />
       <h1>All Movies</h1>
+      <MovieCard
+        awards={awards}
+        companies={companies}
+        contentRating={contentRating}
+        directors={directors}
+        fullTitle={fullTitle}
+        genres={genres}
+        id={id}
+        imDbRating={imDbRating}
+        image={image}
+        languages={languages}
+        plot={plot}
+        releaseDate={releaseDate}
+        stars={stars}
+        // title={title}
+        // trailer={trailer}
+        writers={writers}
+        year={year}
+      />
+      {/* <MovieCard movie={title} /> */}
     </div>
   );
 }
