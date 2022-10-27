814481de6da83546c5da498331610d454176593c
laibasarwar <sarwar.laiba@gmail.com>
2022-10-27 14:46:46 -0400

    took out consolelog

--- src/pages/IndividualBook.js
+++ src/pages/IndividualBook.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
-import { useParams } from "react-router";
+// import { useParams } from "react-router";
 import { BOOKS_APP_API_KEY } from "../API_KEYS";
 import Header from "../components/Header";
 import BookCard from "../components/bookCard";
@@ -14,9 +14,6 @@ function Book() {
   //   const { id } = useParams();
 
   //   const bookData1 = bookData.find((book) => book.id === id);
-  //   console.log("data", bookData1);
-
-  console.log("searchParams", searchParams.get("title"));
 
   useEffect(() => {
     const titleToQuery = searchParams.get("title") || title;
@@ -33,8 +30,6 @@ function Book() {
         setBookData({});
       });
   }, []);
-
-  console.log("Title", bookData);
 
   const {
     author,
@@ -80,39 +75,3 @@ function Book() {
   );
 }
 export default Book;
-
-// import axios from "axios";
-// import React, { useEffect, useState, useMemo } from "react";
-// import { BOOKS_APP_API_KEY } from "../API_KEYS";
-// import BookCard from "../components/bookCard";
-
-// const BOOKSURL = `https://www.googleapis.com/books/v1/users/115218350790390753405/bookshelves/0/volumes?key=${BOOKS_APP_API_KEY}`;
-
-// function Books() {
-//   const [bookData, setBookData] = useState({});
-
-//   useEffect(() => {
-//     axios
-//       .get(
-//         `https://www.googleapis.com/books/v1/users/115218350790390753405/bookshelves/0/volumes?key=${BOOKS_APP_API_KEY}`
-//       )
-//       .then(function (bresponse) {
-//         setBookData(bresponse.data);
-//       })
-//       .catch(function (error) {
-//         setBookData({});
-//       });
-//   }, []);
-
-//   console.log("Book", bookData.items);
-
-//   //10 fav books
-//   //Like a Bird, All My Rage, The Book Thief, Love Letters to the Dead, All the Bright Places, The Kite Runner, A Thousand Splendid Suns, And the Mountains Echoed, Home Fire, A Place for Us
-
-//   return (
-//     <div>
-//       <h1>All Books</h1>
-//     </div>
-//   );
-// }
-// export default Books;
